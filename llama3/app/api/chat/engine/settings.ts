cdc2f7267e342d270cbb544f520a6db7f23ee0d9
ItaloGustavoS <42496107+ItaloGustavoS@users.noreply.github.com>
2024-06-27 09:59:40 -0300

    Changed Context File

--- llama3/app/api/chat/engine/settings.ts
+++ llama3/app/api/chat/engine/settings.ts
@@ -65,7 +65,7 @@ function initOllama() {
     config,
   });
   Settings.embedModel = new HuggingFaceEmbedding({
-    modelType: "BAAI/bge-small-en-v1.5",
+    modelType: "BAAI/bge-m3",
     quantized: false,
   });
 }
--- llama3/app/api/chat/stream-helper.ts
+++ llama3/app/api/chat/stream-helper.ts
@@ -1,3 +1,4 @@
+import * as traceloop from "@traceloop/node-server-sdk";
 import { StreamData } from "ai";
 import {
   CallbackManager,
@@ -6,6 +7,12 @@ import {
   ToolCall,
   ToolOutput,
 } from "llamaindex";
+
+traceloop.initialize({
+  disableBatch: true,
+  apiKey:
+    "d183996b6e5a4462949251b2a8b6b42bdb31656542d72f512a9a606b9741a5df3b4f40e234cdae2d81e7d79b38304bab",
+});
 
 function getNodeUrl(metadata: Metadata) {
   const url = metadata["URL"];
